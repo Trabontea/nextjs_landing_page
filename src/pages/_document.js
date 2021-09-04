c5532f2b71c81d40e8572e39f8c2f4854aa85b85
Trabontea <danielmbotea@gmail.com>
2021-09-04 17:04:31 +0300

    modify gg

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,22 +1,22 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-class CustomDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
 
-  render() {
-    return (
-      <Html lang="en-US">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+    render() {
+        return (
+            <Html>
+                <Head />
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
 
-export default CustomDocument;
+export default MyDocument
